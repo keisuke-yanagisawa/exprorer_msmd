023bc2db1620e982f3fa4addf4347230c5c29e8f
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2022-02-17 23:08:32 +0000

    separate executable class objects

---
+++ script/utilities/executable/tleap.py
@@ -0,0 +1,50 @@
+import tempfile
+import os
+from subprocess import getoutput as gop
+import jinja2
+from utilities import const
+
+
+class TLeap(object):
+    def __init__(self, exe="tleap"):
+        self.exe = exe
+
+    def set(self, template_file, cids, cosolv_paths, frcmods, box_path, size, ssbonds, at):
+        self.template_file = template_file
+        self.cids = cids
+        self.cosolv_paths = cosolv_paths
+        self.frcmods = frcmods
+        self.box_path = box_path
+        self.size = size
+        self.ssbonds = ssbonds
+        self.at = at
+        return self
+
+    def run(self, oprefix):
+        self.oprefix = oprefix
+        self.parm7 = self.oprefix + ".parm7"
+        self.rst7 = self.oprefix + ".rst7"
+
+        _, inputfile = tempfile.mkstemp(prefix=const.TMP_PREFIX, suffix=const.EXT_INP)
+        data = {
+            "LIGAND_PARAM": f"leaprc.{self.at}",
+            "SS_BONDS": zip(self.ssbonds[0::2], self.ssbonds[1::2]),
+            "COSOLVENTS": [{"ID": cid, "PATH": cpath}
+                        for cid, cpath in zip(self.cids, self.cosolv_paths)],
+            "OUTPUT": self.oprefix,
+            "SYSTEM_PATH": self.box_path,
+            "COSOLV_FRCMODS": self.frcmods,
+            "SIZE": self.size
+        }
+
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader("/"))
+        template = env.get_template(self.template_file)
+        with open(inputfile, "w") as fout:
+            fout.write(template.render(data))
+
+        output = gop(f"{self.exe} -f {inputfile}")
+        print(output)
+        final_charge_info = [s.strip() for s in output.split("\n")
+                            if s.strip().startswith("Total unperturbed charge")][0]
+        self._final_charge_value = float(final_charge_info.split()[-1])
+        return self
