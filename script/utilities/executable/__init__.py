7eb8f724c956ab745ca9f60820a31322c77c91d1
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2022-02-22 07:48:09 +0000

    [WIP] postprocessing

--- script/utilities/executable/__init__.py
+++ script/utilities/executable/__init__.py
@@ -1,3 +1,4 @@
 from utilities.executable.packmol import Packmol
 from utilities.executable.parmchk import Parmchk
 from utilities.executable.tleap import TLeap
+from utilities.executable.cpptraj import Cpptraj
