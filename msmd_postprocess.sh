770acb45977dc3fe6b7bc63f77ab01e45e9bc7c4
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2022-02-25 03:10:01 +0000

    genpmap and maxpmap

--- msmd_postprocess.sh
+++ msmd_postprocess.sh
@@ -10,71 +10,22 @@
 
 #### calculation functions ####
 
-postprocess(){
-
+genpmap(){
     local i=$1
-
-    $PYTHON $WORKDIR/script/postprocess_main.py -basedir $ANALYSISDIR/system$i \
+    $PYTHON $WORKDIR/script/genpmap_main.py -basedir $ANALYSISDIR/system$i \
         $protein_param_file $probe_param_file --debug
+}
 
-#     if [ `is_calculated $OUTPUTDIR $i ${map_prefix}_nVH.dx` == 1 ]; then
-#         logging_info "$TARGET_NAME $i : skipped because it has been already calculated"
-#         continue
-#     fi
-
-#     rm -r $OUTPUTDIR/prep$i # remove existing files
-#     mkdir -p $OUTPUTDIR/prep$i/input
-
-#     logging_debug "protein_param_file $protein_param_file"
-#     logging_debug "probe_param_file $probe_param_file"
-
-#     cp $protein_param_file $OUTPUTDIR/prep$i/input/protein.conf
-#     cp $probe_param_file   $OUTPUTDIR/prep$i/input/probe.conf
-#     cp $md_protocol_file   $OUTPUTDIR/prep$i/input/md_protocol.yaml
-#     cp `dirname $protein_param_file`/`get_ini_variable $protein_param_file Protein pdb` $OUTPUTDIR/prep$i/input
-#     cp `dirname $probe_param_file`/`get_ini_variable $probe_param_file Cosolvent mol2` $OUTPUTDIR/prep$i/input
-#     cp `dirname $probe_param_file`/`get_ini_variable $probe_param_file Cosolvent pdb` $OUTPUTDIR/prep$i/input
-    
-#     cd $OUTPUTDIR/prep$i
-
-#     cosolvent_ID=`get_ini_variable $OUTPUTDIR/prep$i/input/probe.conf Cosolvent cid`
-#     $PYTHON $WORKDIR/script/generate_msmd_system.py \
-# 	-prot_param $OUTPUTDIR/prep$i/input/protein.conf \
-# 	-cosolv_param $OUTPUTDIR/prep$i/input/probe.conf \
-# 	-oprefix $OUTPUTDIR/prep$i/${TARGET_NAME}_GMX \
-# 	--seed $i \
-# 	--no-rm-temp
-
-#     # $PYTHON $WORKDIR/script/convert_amber_to_gromacs.py \
-# 	#    -iprefix $OUTPUTDIR/prep$i/${TARGET_NAME} \
-# 	#    -oprefix $OUTPUTDIR/prep$i/${TARGET_NAME}_GMX
-
-#     # add virtual interaction between cosolvents
-#     $PYTHON $WORKDIR/script/addvirtatom2top.py \
-# 	-i $OUTPUTDIR/prep$i/${TARGET_NAME}_GMX.top \
-# 	-o $OUTPUTDIR/prep$i/${TARGET_NAME}_tmp.top \
-# 	-cname ${cosolvent_ID} \
-# 	-ovis $OUTPUTDIR/prep$i/virtual_repulsion.top 
-#     $PYTHON $WORKDIR/script/addvirtatom2gro.py \
-# 	-i $OUTPUTDIR/prep$i/${TARGET_NAME}_GMX.gro \
-# 	-o $OUTPUTDIR/prep$i/$TARGET_NAME.gro \
-# 	-cname ${cosolvent_ID} \
-# 	-vname "VIS"
-
-#     # gen position restraint files
-#     $PYTHON $WORKDIR/script/add_posredefine2top.py \
-# 	-v -res WAT Na+ Cl- CA MG ZN CU ${cosolvent_ID} \
-# 	-target protein \
-# 	-gro $OUTPUTDIR/prep$i/$TARGET_NAME.gro \
-# 	-i $OUTPUTDIR/prep$i/${TARGET_NAME}_tmp.top \
-# 	-o $OUTPUTDIR/prep$i/$TARGET_NAME.top \
-
-
-#     $GMX trjconv -s $OUTPUTDIR/prep$i/$TARGET_NAME.gro -f $OUTPUTDIR/prep$i/$TARGET_NAME.gro -o $OUTPUTDIR/prep$i/$TARGET_NAME.pdb <<EOF
-# 0
-# EOF
-#     # prepared files are:
-#     # xxxx.top, xxxx.gro, virtual_repulsion.top, xxxx.pdb
+maxpmap(){
+    #TODO: map_test_**.dx_pmap.dx is terrible hard-coding
+    $PYTHON $WORKDIR/script/maxpmap_main.py $ANALYSISDIR/system*/map_test_nV.dx_pmap.dx \
+        $ANALYSISDIR/maxPMAP_nV.dx
+    $PYTHON $WORKDIR/script/maxpmap_main.py $ANALYSISDIR/system*/map_test_nVH.dx_pmap.dx \
+        $ANALYSISDIR/maxPMAP_nVH.dx
+    $PYTHON $WORKDIR/script/maxpmap_main.py $ANALYSISDIR/system*/map_test_O.dx_pmap.dx \
+        $ANALYSISDIR/maxPMAP_V.dx
+    $PYTHON $WORKDIR/script/maxpmap_main.py $ANALYSISDIR/system*/map_test_V.dx_pmap.dx \
+        $ANALYSISDIR/maxPMAP_O.dx
 }
 
 #### output environments ####
@@ -99,9 +50,10 @@ WORKDIR=`pwd`
 
 iter_ed=$(( $iter + $iter_st ))
 
-# parallel preparation
 for i in `seq $iter_st $(( $iter_ed - 1 ))`
 do
-    postprocess $i &
+    genpmap $i &
 done
 wait
+
+maxpmap
