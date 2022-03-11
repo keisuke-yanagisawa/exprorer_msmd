45be2d86d0db6a214a3bc6fac70e211f0bde304d
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2022-03-11 07:45:17 +0000

    [WIP] use YAML file instead of config file.

--- example/msmd_config.sh
+++ example/msmd_config.sh
@@ -3,7 +3,7 @@ iter_st=0
 OUTPUTDIR=`pwd`/output/test
 protein_param_file=example/protein.conf
 probe_param_file=example/probe.conf
-md_protocol_file=example/md_protocol.yaml
+md_protocol_file=example/msmd_protocol.yaml
 TARGET_NAME=TEST_PROJECT
 
 # analysis
--- script/utilities/util.py
+++ script/utilities/util.py
@@ -1,11 +1,18 @@
 import random
 import string
-from os.path import expanduser, expandvars
+import os
 
+import yaml
+
+def getabsolutepath(path):
+    path = expandpath(path)
+    if not path.startswith("/"): # relative path
+        path = os.getcwd() + "/" + path
+    return path
 
 def expandpath(path):
-    path = expanduser(path)
-    return expandvars(path)
+    path = os.path.expanduser(path)
+    return os.path.expandvars(path)
 
 
 def randomname(n):
@@ -18,3 +25,61 @@ def dat_dump(dat):
         print("[%s]" % section)
         for option in dat[section]:
             print("  %s : %s" % (option, dat[section][option]))
+
+
+# Notation 1-2 => 1,2  5-9:2 => 5,7,9
+def expand_index(ind_info):
+    # single element will be parsed as integer in yaml.safe_load
+    # Thus this process is needed
+    if type(ind_info) == int:
+        return [ind_info]
+    
+    ret = []
+    elems = ind_info.split(",")
+    for elem in elems:
+        if elem.find("-") == -1:
+            elem = int(elem)
+            ret.append(elem)
+        elif elem.find(":") == -1:
+            st, ed = elem.split("-")
+            st, ed = int(st), int(ed)
+            ret.extend(list(range(st, ed+1)))
+        else:
+            window, offset = elem.split(":")
+            st, ed = window.split("-")
+            st, ed, offset = int(st), int(ed), int(offset)
+            ret.extend(list(range(st, ed+1, offset)))
+    return ret
+
+def parse_yaml(yamlpath):
+    YAML_DIR_PATH = os.path.dirname(
+        getabsolutepath(yamlpath) 
+    )
+    with open(yamlpath) as fin:
+        setting = yaml.safe_load(fin)
+    if not "mol2" in setting["input"]["probe"] \
+       or setting["input"]["probe"]["mol2"] is None:
+        setting["input"]["probe"]["mol2"] \
+            = setting["input"]["probe"]["cid"]+".mol2"
+    if not "pdb" in setting["input"]["probe"] \
+       or setting["input"]["probe"]["pdb"] is None:
+        setting["input"]["probe"]["pdb"] \
+            = setting["input"]["probe"]["cid"]+".pdb"
+
+    setting["general"]["workdir"] = setting["general"]["workdir"] \
+        if setting["general"]["workdir"].startswith("/") \
+        else YAML_DIR_PATH + "/" + setting["general"]["workdir"]
+    setting["input"]["protein"]["pdb"] = setting["input"]["protein"]["pdb"] \
+        if setting["input"]["protein"]["pdb"].startswith("/") \
+        else YAML_DIR_PATH + "/" + setting["input"]["protein"]["pdb"]
+    setting["input"]["probe"]["pdb"] = setting["input"]["probe"]["pdb"] \
+        if setting["input"]["probe"]["pdb"].startswith("/") \
+        else YAML_DIR_PATH + "/" + setting["input"]["probe"]["pdb"]
+    setting["input"]["probe"]["mol2"] = setting["input"]["probe"]["mol2"] \
+        if setting["input"]["probe"]["mol2"].startswith("/") \
+        else YAML_DIR_PATH + "/" + setting["input"]["probe"]["mol2"]
+
+    if setting["input"]["protein"]["ssbond"] is None:
+        setting["input"]["protein"]["ssbond"] = []
+
+    return setting
