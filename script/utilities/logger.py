410aaf5393a7e553fe4e208d237c72eed413273d
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2022-03-13 20:40:14 +0000

    update logging

--- script/utilities/logger.py
+++ script/utilities/logger.py
@@ -2,7 +2,7 @@
 from logging import Formatter, handlers, StreamHandler, getLogger
 from logging import DEBUG, ERROR, WARN, INFO, CRITICAL
 
-__DEFAULT_LOG_LEVEL__ = INFO
+__DEFAULT_LOG_LEVEL__ = WARN
 
 class Logger:
     def __init__(self, path=""):
@@ -11,19 +11,19 @@ class Logger:
         formatter = Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
 
         # stdout
-        handler = StreamHandler()
-        handler.setLevel(__DEFAULT_LOG_LEVEL__)
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        self.handler = StreamHandler()
+        self.handler.setLevel(__DEFAULT_LOG_LEVEL__)
+        self.handler.setFormatter(formatter)
+        self.logger.addHandler(self.handler)
 
         # file
         if(path != ""):
-            handler = handlers.RotatingFileHandler(filename = path,
+            self.handler = handlers.RotatingFileHandler(filename = path,
                                                    maxBytes = 1048576,
                                                    backupCount = 3)
-            handler.setLevel(__DEFAULT_LOG_LEVEL__)
-            handler.setFormatter(formatter)
-            self.logger.addHandler(handler)
+            self.handler.setLevel(__DEFAULT_LOG_LEVEL__)
+            self.handler.setFormatter(formatter)
+            self.logger.addHandler(self.handler)
 
     def debug(self, msg):
         self.logger.debug(msg)
@@ -43,15 +43,21 @@ class Logger:
     def setLevel(self, level):
         if level.lower() == "debug":
             self.logger.setLevel(DEBUG)
+            self.handler.setLevel(DEBUG)
+            self.debug("logging level: DEBUG")
         elif level.lower() == "info":
             self.logger.setLevel(INFO)
+            self.handler.setLevel(INFO)
         elif level.lower() == "warn" \
             or level.lower() == "warning":
             self.logger.setLevel(WARN)
+            self.handler.setLevel(WARN)
         elif level.lower() == "error":
             self.logger.setLevel(ERROR)
+            self.handler.setLevel(ERROR)
         elif level.lower() == "critical":
             self.logger.setLevel(CRITICAL)
+            self.handler.setLevel(CRITICAL)
         else:
             self.warn(f"level '{level}' is invalid.")
             self.warn(f"logger level has not been changed.")
