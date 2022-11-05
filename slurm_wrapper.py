bd68acc1edf123ae0f6b11740d67d5b6c4d019e1
Keisuke Yanagisawa <yanagisawa@bi.cs.titech.ac.jp>
2022-11-05 15:38:40 +0900

    update

--- slurm_wrapper.py
+++ slurm_wrapper.py
@@ -2,30 +2,12 @@ import argparse
 import os
 from script.utilities.logger import logger
 from script.utilities import util
+import yaml
+from string import Template
 
-slurm_template="""#!/usr/bin/bash
-#SBATCH -J {JOB_NAME}
-#SBATCH -p {PARTITION}
-#SBATCH -N 1
-#SBATCH --gres=gpu:1
-#SBATCH --get-user-env
-#SBATCH -o {JOB_NAME}.out
-#SBATCH -e {JOB_NAME}.err
-
-EXPRORER_MSMD="/mnt/fs/yanagisawa/workspace/9999_git_repositories/exprorer_msmd/exprorer_msmd"
-MSMD_OPTIONS="--iter-index {ITER_INDEX}"
-SETTING_YAML={SETTING_YAML}
-
-#IF SINGULARITY
-module load singularity
-SINGULARITY_OPTIONS="--bind /mnt/ghosts:/mnt/ghosts --bind /mnt/fs:/mnt/fs --bind /tmp:/tmp"
-SINGULARITY_SIF="/mnt/fs/yanagisawa/workspace/9999_git_repositories/exprorer_msmd/.devcontainer/Singularity.sif"
-SINGULARITY="singularity exec --nv $SINGULARITY_OPTIONS $SINGULARITY_SIF"
-
-echo $SINGULARITY $EXPRORER_MSMD $MSMD_OPTIONS $SETTING_YAML
-$SINGULARITY $EXPRORER_MSMD $MSMD_OPTIONS $SETTING_YAML
-"""
-
+def read_yaml(path):
+    with open(path, "r") as f:
+        return yaml.safe_load(f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -39,6 +21,8 @@ if __name__ == "__main__":
     parser.add_argument("-N,--run-per-job", dest="run_per_job", type=int, default=1)
     args = parser.parse_args()
 
+    environment = read_yaml(os.path.dirname(util.getabsolutepath(__file__))+"/environment.yaml")
+
     # initial logger level is "warn"
     if args.debug:
         logger.setLevel("debug")
@@ -50,20 +34,55 @@ if __name__ == "__main__":
     indices = list(set(util.expand_index(setting["general"]["iter_index"])))    
     workdir = setting["general"]["workdir"]
 
+    GROUP = ""
+    QTYPE = ""
+    MAXTIME = ""
+    if environment["use_scheduler"] == "True":
+        GROUP = environment["scheduler"]["group"]
+        QTYPE = environment["scheduler"]["qtype"]
+        MAXTIME = environment["scheduler"]["maxtime"]
+
+    use_singularity = environment["use_singularity"]
+    singularity_sifpath        = ""
+    singularity_bind           = ""
+    singularity_prerequirement = ""
+    if use_singularity == True:
+        singularity_sifpath = environment["singularity"]["sifpath"]
+        singularity_prerequirement = environment["singularity"]["prerequirement"]
+        singularity_bind = environment["singularity"]["bind"]
+
+
+    template = ""
+    if environment["use_scheduler"] != True:
+        template = open(os.path.dirname(util.getabsolutepath(__file__))+"/template/pbs", "r").read()
+    elif environment["scheduler"]["type"] == "PBS":
+        template = open(os.path.dirname(util.getabsolutepath(__file__))+"/template/pbs", "r").read()
+    elif environment["scheduler"]["type"] == "Slurm":
+        template = open(os.path.dirname(util.getabsolutepath(__file__))+"/template/slurm", "r").read()
+
     import math
     njobs = math.ceil(len(indices) / args.run_per_job)
     for i, gr in enumerate([indices[i::njobs] for i in range(njobs)]):
         JOB_NAME = setting["general"]["name"]
-        PARTITION = "defq"
-        ITER_INDEX = ",".join([str(i) for i in gr])
+        ITER_INDEX = ",".join([str(j) for j in gr])
         SETTING_YAML = args.setting_yaml
 
         # generate slurm script
-        with open(f"{workdir}/slurm{i}.sh", "w") as fout:
-            fout.write(slurm_template.format(**{
-                "JOB_NAME": f"JOB{i}_{JOB_NAME}",
-                "PARTITION": PARTITION,
+        os.system(f"mkdir -p {workdir}")
+        with open(f"{workdir}/JOB{i}.sh", "w") as fout:
+            fout.write(template.format(**{
+                "GROUP": GROUP,
+                "QTYPE": QTYPE,
+                "runID": f"{i}",
+                "JOB_NAME": JOB_NAME,
+                "TIME_LENGTH": MAXTIME,
+                "use_singularity": use_singularity,
+                "singularity_prerequirement": singularity_prerequirement,
+                "singularity_sifpath": singularity_sifpath,
+                "singularity_bind": singularity_bind,
+                "PATH_EXPRORER_MSMD": os.path.dirname(util.getabsolutepath(__file__))+"/exprorer_msmd",
                 "ITER_INDEX": ITER_INDEX,
-                "SETTING_YAML": SETTING_YAML,
+                "YAML": SETTING_YAML,
+                "NGPUS": len(gr)
             }))
-        os.system(f"sbatch {workdir}/slurm{i}.sh")
+        os.system(f"sbatch {workdir}/JOB{i}.sh")
