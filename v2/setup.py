e1c121b4536f2f90e380a32d611961954b27ee2b
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-02-27 09:08:57 +0000

    add installation system

---
+++ v2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ExprorerMSMD',
+    packages=find_packages(),
+    version='',
+    description='Python library to run and analyze mixed-solvent molecular dynamics simulations',
+    author='',
+    license='',
+)
