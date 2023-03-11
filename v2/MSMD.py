69118f5a741f44a58266f3dc22cd4fd655c80ffc
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-03-11 02:55:05 +0000

    [WIP] commit

--- v2/MSMD.py
+++ v2/MSMD.py
@@ -1,12 +1,15 @@
 class MSMDSystemGenerator:
     """与えられた入力から、OpenMMのSystemを作るジェネレータクラス。"""
+
     def __init__(self):
         pass
+
 
 class SimulationSequence:
     """
     複数ステップのシミュレーションをまとめるクラス。
     json形式のファイルを入力として受け取ることで、Sequenceを自動的に作成する。
     """
+
     def __init__(self):
         pass
