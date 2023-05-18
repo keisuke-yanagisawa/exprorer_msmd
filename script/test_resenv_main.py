8e432cb5b2751184b564ffb1cde69860d9c8a4d0
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-05-18 08:03:57 +0000

    uses resenv as module not as executable

---
+++ script/test_resenv_main.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from script.resenv_main import resenv
+
+class TestResenvMain(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestResenvMain, self).__init__(*args, **kwargs)
+        self.gridfile = "script/test_data/maxpmap_for_resenv.dx"
+        self.trajectories = ["script/test_data/trajectory_for_resenv.pdb"]
+        self.resn = "A11"
+
+
+    def test_resenv(self):
+        resenv(self.gridfile, self.trajectories, self.resn, ".tmp.pdb",
+               threshold=0.001)
+        
+        resenv_str = open(".tmp.pdb").read()
+        expected = open("script/test_data/resenv_expected.pdb").read()
+        self.assertEqual(resenv_str, expected)
+
