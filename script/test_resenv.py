4a29e84acc92d32b23f7c6e6159e7286ac8acc8a
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-05-28 04:39:28 +0000

    add test codes of resenv

--- script/test_resenv.py
+++ script/test_resenv.py
@@ -5,17 +5,44 @@ import tempfile
 
 class TestResenvMain(TestCase):
     def __init__(self, *args, **kwargs):
-        super(TestResenvMain, self).__init__(*args, **kwargs)
+class TestResenv(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestResenv, self).__init__(*args, **kwargs)
         self.gridfile = "script/test_data/maxpmap_for_resenv.dx"
         self.trajectories = ["script/test_data/trajectory_for_resenv.pdb"]
         self.expected_resenv_pdb = "script/test_data/resenv_expected.pdb"
         self.resn = "A11"
+        _, self.outputpdb = tempfile.mkstemp(suffix=".pdb")
 
-    def test_resenv(self):
-        _, tmppdb = tempfile.mkstemp(suffix=".pdb")
-        resenv(self.gridfile, self.trajectories, self.resn, tmppdb,
+    def test_normal_case(self):
+        resenv(self.gridfile, self.trajectories, self.resn, self.outputpdb,
                threshold=0.001)
 
-        resenv_str = open(tmppdb).read()
+        resenv_str = open(self.outputpdb).read()
         expected = open(self.expected_resenv_pdb).read()
         self.assertEqual(resenv_str, expected)
+
+    def test_grid_file_is_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            resenv("NOTHING", self.trajectories, self.resn, self.outputpdb)
+
+    def test_ipdb_file_is_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            resenv(self.gridfile, ["NOTHING"], self.resn, self.outputpdb)
+
+    def test_one_of_ipdb_file_is_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            resenv(self.gridfile, [self.trajectories[0], "NOTHING"], self.resn, self.outputpdb)
+
+    def test_invalid_resn(self):
+        with self.assertRaises(ValueError):
+            resenv(self.gridfile, self.trajectories, "INVALID_RESN", self.outputpdb)
+
+    def test_output_file_cannot_be_created(self):
+        with self.assertRaises(FileNotFoundError):
+            resenv(self.gridfile, self.trajectories, self.resn, "/INVALID/PATH/TO/OUTPUT")
+
+    def test_no_output_structure(self):
+        pass
+        # with self.assertRaises(FileNotFoundError):
+        #     resenv(self.gridfile, self.trajectories, "INVALID_RESN", None)
