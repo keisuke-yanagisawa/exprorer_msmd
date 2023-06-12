d61f6fb3dc2b4573e78f557bd8ef3f35835c196e
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-06-12 23:48:29 +0000

    threshold is not parameterable

--- script/utilities/util.py
+++ script/utilities/util.py
@@ -74,6 +74,8 @@ def set_default(setting: dict) -> None:
         setting["general"]["multiprocessing"] = True
     if "valid_dist" not in setting["map"]:
         setting["map"]["valid_dist"] = 5
+    if "threshold" not in setting["probe_profile"]:
+        setting["probe_profile"]["threshold"] = 0.001
     if "env_dist" not in setting["probe_profile"]:
         setting["probe_profile"]["env_dist"] = 4
     if "dt" not in setting["exprorer_msmd"]["general"]:
