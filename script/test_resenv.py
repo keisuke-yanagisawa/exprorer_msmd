bf6f8d1a9b94dcee1ef9b31117e233f360a9ae10
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-06-18 23:43:53 +0000

    grid object should be given to resenv()

--- script/test_resenv.py
+++ script/test_resenv.py
@@ -3,19 +3,21 @@ from script.resenv import resenv
 import tempfile
 from script.utilities.Bio import PDB as uPDB
 import numpy as np
+import gridData
 
 
 class TestResenv(TestCase):
     def __init__(self, *args, **kwargs):
         super(TestResenv, self).__init__(*args, **kwargs)
         self.gridfile = "script/test_data/maxpmap_for_resenv.dx"
-        self.trajectories = ["script/test_data/trajectory_for_resenv.pdb"]
+        self.grid = gridData.Grid(self.gridfile)
+        self.trajectory_files = ["script/test_data/trajectory_for_resenv.pdb"]
         self.expected_resenv_pdb = "script/test_data/resenv_expected.pdb"
         self.resn = "A11"
         _, self.outputpdb = tempfile.mkstemp(suffix=".pdb")
 
     def test_normal_case(self):
-        resenv(self.gridfile, self.trajectories, self.resn, [" CB "], self.outputpdb,
+        resenv(self.grid, self.trajectory_files, self.resn, [" CB "], self.outputpdb,
                threshold=0.001)
 
         struct = uPDB.get_structure(self.outputpdb)
@@ -24,27 +26,23 @@ class TestResenv(TestCase):
         for model, expected_model in zip(struct, expected_struct):  # type: ignore
             np.testing.assert_array_almost_equal(uPDB.get_attr(model, "coord"), uPDB.get_attr(expected_model, "coord"))
 
-    def test_grid_file_is_not_found(self):
-        with self.assertRaises(FileNotFoundError):
-            resenv("NOTHING", self.trajectories, self.resn, [" CB "], self.outputpdb)
-
     def test_ipdb_file_is_not_found(self):
         with self.assertRaises(FileNotFoundError):
-            resenv(self.gridfile, ["NOTHING"], self.resn, [" CB "], self.outputpdb)
+            resenv(self.grid, ["NOTHING"], self.resn, [" CB "], self.outputpdb)
 
     def test_one_of_ipdb_file_is_not_found(self):
         with self.assertRaises(FileNotFoundError):
-            resenv(self.gridfile, [self.trajectories[0], "NOTHING"], self.resn, [" CB "], self.outputpdb)
+            resenv(self.grid, [self.trajectory_files[0], "NOTHING"], self.resn, [" CB "], self.outputpdb)
 
     def test_invalid_resn(self):
         with self.assertRaises(ValueError):
-            resenv(self.gridfile, self.trajectories, "INVALID_RESN", [" CB "], self.outputpdb)
+            resenv(self.grid, self.trajectory_files, "INVALID_RESN", [" CB "], self.outputpdb)
 
     def test_output_file_cannot_be_created(self):
         with self.assertRaises(FileNotFoundError):
-            resenv(self.gridfile, self.trajectories, self.resn, [" CB "], "/INVALID/PATH/TO/OUTPUT")
+            resenv(self.grid, self.trajectory_files, self.resn, [" CB "], "/INVALID/PATH/TO/OUTPUT")
 
     def test_no_output_structure(self):
         pass
         # with self.assertRaises(FileNotFoundError):
-        #     resenv(self.gridfile, self.trajectories, "INVALID_RESN", None)
+        #     resenv(self.grid, self.trajectories, "INVALID_RESN", None)
