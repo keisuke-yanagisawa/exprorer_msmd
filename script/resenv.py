7c955503a2c1845ea00bb90e93bc08a7c913fb40
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-06-18 23:43:53 +0000

    grid object should be given to resenv()

--- script/resenv.py
+++ script/resenv.py
@@ -6,7 +6,7 @@ from scipy .spatial import distance
 from tqdm import tqdm
 from joblib import Parallel, delayed
 from Bio.PDB.Model import Model
-from Bio.PDB.Atom import Atom
+from Bio.PDB.Structure import Structure
 from script.utilities.Bio import PDB as uPDB
 
 VERSION = "0.3.0"
@@ -46,11 +46,6 @@ def compute_SR_probe_resis(model: Model,
                           sele=lambda a: uPDB.get_resname(a) == resn and not uPDB.is_hydrogen(a))
     coords = uPDB.get_attr(model, "coord",
                            sele=lambda a: uPDB.get_resname(a) == resn and not uPDB.is_hydrogen(a))
-    # below interpolation is "3D-spline" interpolation. It shows undesirable behavior
-    # XX, YY, ZZ = np.array(coords).T
-    # values = dx.interpolated(XX, YY, ZZ)
-
-    # Because of the reason, we changed to "nearest" interpolation to obtain stable results
     interp = RegularGridInterpolator(dx.midpoints, dx.grid, method="nearest", fill_value=-1, bounds_error=False)
     values = interp(np.array(coords))
 
@@ -75,51 +70,52 @@ def __get_surrounded_resis_around_a_residue(model: Model,
     return set(environment_resis)
 
 
-def __wrapper(model: Model,
+def __wrapper(model_wo_water: Model,
               dx: gridData.Grid,
               focused_resname: str,
-              protein_residue_atomnames: List[str],
+              res_atomnames: List[str],
               threshold: float,
               lt: bool,
               env_distance: float):
-
-    # water_resis = set(
-    #     uPDB.get_attr(model, "resid", sele=uPDB.is_water)
-    # )
     focused_residue_resis = set(
-        uPDB.get_attr(model, "resid", sele=lambda a: uPDB.get_resname(a) == focused_resname)
+        uPDB.get_attr(model_wo_water, "resid", sele=lambda a: uPDB.get_resname(a) == focused_resname)
     )
 
-    resi_set = compute_SR_probe_resis(model, dx, focused_resname, threshold, lt)
+    resi_set = compute_SR_probe_resis(model_wo_water, dx, focused_resname, threshold, lt)
 
     ret_env_structs = []
     for resi in resi_set:
         exclude_resis = focused_residue_resis
-        environment_resis = __get_surrounded_resis_around_a_residue(model, resi, env_distance)
+        environment_resis = __get_surrounded_resis_around_a_residue(model_wo_water, resi, env_distance)
         environment_resis -= exclude_resis
 
         if len(environment_resis) == 0:
             continue
 
         sele = uPDB.Selector(lambda a: uPDB.get_resi(a) in (environment_resis | set([resi])))
-        env_struct = uPDB.extract_substructure(model, sele)
+        env_struct = uPDB.extract_substructure(model_wo_water, sele)
         ret_env_structs.append(env_struct)
     return ret_env_structs
 
 
-def resenv(grid: str, ipdb: List[str], resn: str, res_atomnames: List[str], opdb: str,
+def resenv(grid: gridData.Grid, ipdb: List[str], resn: str, res_atomnames: List[str], opdb: str,
            threshold: float = 0.2, lt: bool = False,
-           env_distance: float = 4, n_jobs: int = 1, verbose=False):
-    dx = gridData.Grid(grid)
+           env_distance: float = 4, verbose=False) -> Structure:
+    """
+    Extract probe which is on high-probability region with its environment (protein residues)
+    """
+    dx = grid
 
     out_helper = uPDB.PDBIOhelper(opdb)
     for path in ipdb:
         reader = uPDB.MultiModelPDBReader(path)
 
+        # lst_of_lst = [__wrapper(model, dx, resn, res_atomnames, threshold, lt, env_distance)
+        #               for model in tqdm(reader, desc="[extract res. env.]", disable=not verbose)]
         lst_of_lst = Parallel(n_jobs=1)(
             delayed(__wrapper)(model, dx, resn, res_atomnames, threshold, lt, env_distance)
             for model in tqdm(reader, desc="[extract res. env.]", disable=not verbose)
-        )
+        )  # now parallel processing is disabled
 
         if lst_of_lst is None:
             continue
@@ -127,7 +123,7 @@ def resenv(grid: str, ipdb: List[str], resn: str, res_atomnames: List[str], opdb
         for lst in lst_of_lst:
             for struct in lst:
                 out_helper.save(struct)
-    out_helper.close()
+    out_helper.close()  # it is necessary to read the output file below
 
     structs = uPDB.get_structure(opdb)
     if (len(structs) == 0):
