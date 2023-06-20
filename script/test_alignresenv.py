c476677c99f23faa397f3521023d904808fa0869
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2023-06-20 01:47:20 +0000

    update test cases

--- script/test_alignresenv.py
+++ script/test_alignresenv.py
@@ -14,30 +14,22 @@ class TestAlignResEnv(TestCase):
 
     def test_single_structure(self):
         alignresenv.align_res_env(
-            [self.single_atom_struct],
+            self.single_atom_struct,
             self.single_atom_struct[0],
             "VAL"
         )
 
     def test_single_structure_with_multiple_models(self):
         alignresenv.align_res_env(
-            [self.two_models_struct],
+            self.two_models_struct,
             self.two_models_struct[0],
             "PRO"
         )
 
-    def test_no_structures(self):
-        with self.assertRaises(ValueError):
-            alignresenv.align_res_env(
-                [],
-                self.single_atom_struct[0],
-                "VAL"
-            )
-
     def test_structure_with_no_atom_model(self):
         with self.assertRaises(ValueError):
             alignresenv.align_res_env(
-                [self.no_atom_struct],
+                self.no_atom_struct,
                 self.single_atom_struct[0],
                 "VAL"
             )
@@ -45,7 +37,7 @@ class TestAlignResEnv(TestCase):
     def test_Reference_does_not_have_any_atom(self):
         with self.assertRaises(ValueError):
             alignresenv.align_res_env(
-                [self.two_models_struct],
+                self.two_models_struct,
                 self.no_atom_struct[0],
                 "UNK",
                 focused=[" CA "]
@@ -53,23 +45,15 @@ class TestAlignResEnv(TestCase):
 
     def test_focused_stable(self):
         alignresenv.align_res_env(
-            [self.two_models_struct],
+            self.two_models_struct,
             self.two_models_struct[0],
             "PRO",
             focused=[" CA ", " N  ", " C  "]
         )
 
-    def test_multiple_structures(self):
-        alignresenv.align_res_env(
-            [self.single_atom_struct, self.two_atoms_struct],
-            self.single_atom_struct[0],
-            "VAL",
-            focused=[" CA "]
-        )
-
     def test_focused_unstable(self):
         alignresenv.align_res_env(
-            [self.two_models_struct],
+            self.two_models_struct,
             self.two_models_struct[0],
             "PRO",
             focused=[" CA "]
@@ -78,7 +62,7 @@ class TestAlignResEnv(TestCase):
     def test_focused_is_not_in_struct(self):
         with self.assertRaises(ValueError):
             alignresenv.align_res_env(
-                [self.two_models_struct],
+                self.two_models_struct,
                 self.two_models_struct[0],
                 "PRO",
                 focused=[" XX "]
@@ -87,7 +71,7 @@ class TestAlignResEnv(TestCase):
     def test_resn_not_in_structs(self):
         with self.assertRaises(ValueError):
             alignresenv.align_res_env(
-                [self.two_models_struct],
+                self.two_models_struct,
                 self.two_models_struct[0],
                 "UNK",
                 focused=[" CA "]
@@ -95,7 +79,7 @@ class TestAlignResEnv(TestCase):
 
     def test_aligned_coords(self):
         struct = alignresenv.align_res_env(
-            [self.two_atoms_struct],
+            self.two_atoms_struct,
             self.single_atom_struct[0],
             "VAL",
             focused=[" CA "]
